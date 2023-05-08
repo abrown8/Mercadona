fa35f86b313041035e78650edad37540b9d5f497
abrown8 <adrien.brown98@gmail.com>
2023-05-08 17:13:19 +0200

    Tentative de deploiement 12

--- src/app/server.js
+++ src/app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/mercanoda-app"));
+app.use(express.static(__dirname + "/dist/mercadona-app"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/mercadona-app/index.html"));
